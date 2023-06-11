961636432b9a62549cb78f237eefcd3246380b2b
harriszk <zacharykeatonharris@gmail.com>
2023-06-10 23:17:07 -0400

    Implemented mergeAllChunks

--- src/main/java/KWayMerge.java
+++ src/main/java/KWayMerge.java
@@ -4,21 +4,17 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class KWayMerge {
     private static final int MAX_SIZE = 3;
-    private List<Queue<Integer>> chunks;
-    private List<ChunkProcessor> processors;
-
-    private List<Chunk> tempName;
-
+    private Queue<Chunk> chunksToMerge;
     public KWayMerge()
     {
-        this.chunks = new ArrayList<>();
-        this.processors = new ArrayList<>();
+        this.chunksToMerge = new LinkedList<>();
     } // end default construtor
 
     public void mergeAllChunks(List<String> chunkLocations)
@@ -30,17 +26,20 @@ public class KWayMerge {
         // from its file. If there are no more elements in the file then we will no longer
         // check to add more files to that chunks list.
 
+        
+
         List<Integer> loadedChunk;
-        ChunkProcessor processor;
+
         PriorityQueue<Integer> chunkQueue;
-
+        ChunkProcessor chunkProcessor;
+        Chunk chunk;
+        
         // Load in initial chunks into queues 
-        for(String chunk : chunkLocations)
+        for(String chunkLocation : chunkLocations)
         {
-            processor = new ChunkProcessor(chunk);
-            this.processors.add(processor);
+            chunkProcessor = new ChunkProcessor(chunkLocation);
 
-            loadedChunk = processor.loadNElements(MAX_SIZE);
+            loadedChunk = chunkProcessor.loadNElements(MAX_SIZE);
             if(loadedChunk.get(0) == null)
             {
                 break;
@@ -48,69 +47,74 @@ public class KWayMerge {
 
             System.out.println("Loading chunk " + loadedChunk);
 
-            chunkQueue = new PriorityQueue<>(MAX_SIZE);
-            for(Integer element : loadedChunk)
-            {
-                chunkQueue.add(element);
-            } // end for
-            this.chunks.add(chunkQueue);
+            chunkQueue = new PriorityQueue<>(loadedChunk);
+            
+            chunk = new Chunk(chunkQueue, chunkProcessor);
+            this.chunksToMerge.add(chunk);
         } // end for
+
+        // Merge chunks until only one chunk remains
+        while(this.chunksToMerge.size() > 1) 
+        {
+            Chunk left = this.chunksToMerge.poll();
+            Chunk right = this.chunksToMerge.poll();
+
+            String mergedChunkLocation = "tmp/chunks/testChunks" + "/merged_chunk_" + System.currentTimeMillis() + ".txt";
+            merge(left, right, mergedChunkLocation);
+
+            chunkProcessor = new ChunkProcessor(mergedChunkLocation);
+            chunkQueue = new PriorityQueue<>(chunkProcessor.loadNElements(MAX_SIZE));
+            Chunk mergedChunk = new Chunk(chunkQueue, chunkProcessor);
+            this.chunksToMerge.add(mergedChunk);
+        } // end while
+
     } // end mergeAllChunks
 
-    private void merge(int chunkIndex1, int chunkIndex2, String outputFile) 
+    private void merge(Chunk left, Chunk right, String outputFile) 
     {
-        try(FileWriter fileWriter = new FileWriter(outputFile, true)) {
-            while(!this.chunks.get(chunkIndex1).isEmpty() && !this.chunks.get(chunkIndex2).isEmpty()) 
+        try {
+            FileWriter fileWriter = new FileWriter(outputFile, true);
+            
+            while(!left.getQueue().isEmpty() && !right.getQueue().isEmpty())
             {
-                System.out.println(this.chunks.get(chunkIndex1));
-                System.out.println(this.chunks.get(chunkIndex2));
-
-                if(this.chunks.get(chunkIndex1).peek() <= this.chunks.get(chunkIndex2).peek()) {
-                    fileWriter.write(this.chunks.get(chunkIndex1).poll().toString());
+                if(left.getQueue().peek() <= right.getQueue().peek()) {
+                    fileWriter.write(left.getQueue().poll().toString());
                     fileWriter.write(System.lineSeparator());
 
-                    Integer nextElement = this.processors.get(chunkIndex1).loadNElements(1).get(0);
-
-                    //System.out.println(nextElement);
+                    Integer nextElement = left.getProcessor().loadNElements(1).get(0);
                     if(nextElement != null)
                     {
-                        this.chunks.get(chunkIndex1).add(nextElement);
-                    }
+                        left.getQueue().add(nextElement);
+                    } // end if
                 } else {
-                    fileWriter.write(this.chunks.get(chunkIndex2).poll().toString());
+                    fileWriter.write(right.getQueue().poll().toString());
                     fileWriter.write(System.lineSeparator());
 
-                    Integer nextElement = this.processors.get(chunkIndex2).loadNElements(1).get(0);
-
-                    //System.out.println(nextElement);
+                    Integer nextElement = right.getProcessor().loadNElements(1).get(0);
                     if(nextElement != null)
                     {
-                        this.chunks.get(chunkIndex2).add(nextElement);
-                    }
+                        right.getQueue().add(nextElement);
+                    } // end if
                 } // end if
             } // end while
 
-            System.out.println("Out of while");
-            System.out.println(this.chunks.get(chunkIndex1));
-            System.out.println(this.chunks.get(chunkIndex2));
-
             // Write the remaining elements from chunk1 or chunk2
-            while(!this.chunks.get(chunkIndex1).isEmpty()) 
+            while(!left.getQueue().isEmpty()) 
             {
-                fileWriter.write(this.chunks.get(chunkIndex1).poll().toString());
+                fileWriter.write(left.getQueue().poll().toString());
                 fileWriter.write(System.lineSeparator());
             } // end while
 
-            while(!this.chunks.get(chunkIndex2).isEmpty()) 
+            while(!right.getQueue().isEmpty()) 
             {
-                fileWriter.write(this.chunks.get(chunkIndex2).poll().toString());
+                fileWriter.write(right.getQueue().poll().toString());
                 fileWriter.write(System.lineSeparator());
             } // end while
 
-            fileWriter.flush();
+            //fileWriter.flush();
+            fileWriter.close();
         } catch (IOException e) {
             e.printStackTrace();
         } // end try/catch
     } // end merge
-
 } // end KWayMerge
